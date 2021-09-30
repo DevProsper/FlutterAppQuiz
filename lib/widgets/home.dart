f477d23a3db1a971750135f2d771736a1a7037f9
DevProsper <sam@google.com>
2021-09-30 03:00:09 +0100

    Préparation des questions

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'CustomText.dart';
+import 'page_quiz.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -40,7 +41,9 @@ class _MyHomePageState extends State<MyHomePage>{
             new RaisedButton(
               color: Colors.blue,
               onPressed: (){
-
+                Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context){
+                    return new PageQuiz();
+                }));
             },
               child: new CustomText("Commencer le quiz", factor: 1.5),
             ),
